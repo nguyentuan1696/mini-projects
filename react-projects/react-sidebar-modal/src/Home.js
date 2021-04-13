2d475d9a4e205ddda9bf6346a61c46f098f15cf5
nguyentuan1696 <nguyentuan1696@gmail.com>
2021-04-13 23:30:59 +0700

    update

---
+++ react-projects/react-sidebar-modal/src/Home.js
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react'
+import { FaBars } from 'react-icons/fa'
+import {AppContext, useGlobalContext} from './context'
+const Home = () =>
+{
+  const data = useGlobalContext()
+  return (
+    <main>
+      <button className='sidebar-toggle'>
+        <FaBars />
+      </button>
+      <button className='btn'>
+show modal
+      </button>
+    </main>
+  )
+}
+
+export default Home
