331c55bfee29ad8300aa57bd5b625cbaa645dfab
nguyentuan1696 <nguyentuan1696@gmail.com>
2021-04-13 16:48:03 +0700

    update

--- react-projects/starter-project-redux-tutorial-cart/src/reducer.js
+++ react-projects/starter-project-redux-tutorial-cart/src/reducer.js
@@ -8,7 +8,18 @@ const reducer = (state, action) =>
   }
 
   if(action.type === DECREASE) {
-    console.log('You decrease amount!')
+    let tempCart = []
+    if (action.payload.amount === 1) {
+       tempCart = state.cart.filter(cartItem => cartItem.id !== action.payload.id)
+    } else {
+      tempCart = state.cart.map((cartItem) => {
+        if (cartItem.id === action.payload.id) {
+          cartItem = { ...cartItem, amount: cartItem.amount - 1 }
+        }
+        return cartItem
+      })
+     }
+    return { ...state, cart: tempCart }
   }
 
   if (action.type === INCREASE) {
