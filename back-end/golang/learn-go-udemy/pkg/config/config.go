78a109d26d3c4d192d737c6aa1ef9c142fba372d
nguyentuan1696 <nguyentuan1696@gmail.com>
2021-05-06 17:36:51 +0700

    update

--- back-end/golang/learn-go-udemy/pkg/config/config.go
+++ back-end/golang/learn-go-udemy/pkg/config/config.go
@@ -2,10 +2,12 @@ package config
 
 import (
 	"html/template"
+	"log"
 )
 
-
-// AppConfig hold the App configation
+// AppConfig holds the application config
 type AppConfig struct {
+	UseCache      bool
 	TemplateCache map[string]*template.Template
+	InfoLog       *log.Logger
 }
--- back-end/golang/learn-go-udemy/pkg/render/render.go
+++ back-end/golang/learn-go-udemy/pkg/render/render.go
@@ -9,29 +9,44 @@ import (
 	"path/filepath"
 
 	"github.com/nguyentuan1696/learn-golang/pkg/config"
+	"github.com/nguyentuan1696/learn-golang/pkg/models"
 )
 
 var functions = template.FuncMap{}
+
 var app *config.AppConfig
 
-// NewTemplates set the config for the templates package
+// NewTemplates sets the config for the template package
 func NewTemplates(a *config.AppConfig) {
 	app = a
 }
 
-// RenderTemplate renders a template
-func RenderTemplate(w http.ResponseWriter, tmpl string) {
+func AddDefaultData(td *models.TemplateData) *models.TemplateData {
 
-	tc := app.TemplateCache
+	return td
+}
+
+// RenderTemplate renders a template
+func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+	var tc map[string]*template.Template
+
+	if app.UseCache {
+		// get the template cache from the app config
+		tc = app.TemplateCache
+	} else {
+		tc, _ = CreateTemplateCache()
+	}
 
 	t, ok := tc[tmpl]
 	if !ok {
-		log.Fatal("could not get template from template cache")
+		log.Fatal("Could not get template from template cache")
 	}
 
 	buf := new(bytes.Buffer)
 
-	_ = t.Execute(buf, nil)
+	td = AddDefaultData(td)
+
+	_ = t.Execute(buf, td)
 
 	_, err := buf.WriteTo(w)
 	if err != nil {
