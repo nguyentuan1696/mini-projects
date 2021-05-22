3f67785266e5ef2c5f62db3a60556f69d2747bb3
nguyentuan1696 <nguyentuan1696@gmail.com>
2021-05-22 11:45:00 +0700

    update

--- back-end/golang/go-fiber-jwt/handler/auth.go
+++ back-end/golang/go-fiber-jwt/handler/auth.go
@@ -1,5 +1,32 @@
 package handler
 
-func CheckPasswordHash() {
+import (
+	"time"
+
+	"github.com/nguyentuan1696/go-fiber-jwt/config"
+	"github.com/nguyentuan1696/go-fiber-jwt/database"
+	"github.com/nguyentuan1696/go-fiber-jwt/model"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gofiber/fiber/v2"
+	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func getUserByEmail(e string) (*model.User, error) {
+	db := database.DB
+
+	var user model.User
 	
+	if err := db.Where(&model.User{Email:e}).Find(&user).Error; err != nil {
+		if gorm.IsRecordNotFoundError(err) {
+			return nil, nil
+		}
+	}
+	return &user, nil
 }
